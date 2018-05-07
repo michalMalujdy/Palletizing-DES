b69b10734c8646e474e09e5627b02d7dbab8156d
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-05-07 18:36:03 +0200

    Add JsonConfigService

---
+++ TS.Infrastructure/Services/JsonConfigService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using TS.Core.Json;
+
+namespace TS.Infrastructure.Services
+{
+    public class JsonConfigService
+    {
+        public StatesNetJson ReadConfig(string filename)
+        {
+            var json = File.ReadAllText(filename);
+            return JsonConvert.DeserializeObject<StatesNetJson>(json);
+        }
+    }
+}
