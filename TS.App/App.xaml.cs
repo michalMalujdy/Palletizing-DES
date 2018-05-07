ebcd05da9f08b4be8ebed6d633e7e8aaf4e1b19b
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-05-07 19:26:40 +0200

    Set StatesNetView as the starting view

--- TS.App/App.xaml.cs
+++ TS.App/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using TS.App.Views;
 using TS.Infrastructure.AppStart;
 
 namespace TS.App
@@ -69,7 +70,7 @@ namespace TS.App
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(StatesNetView), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
