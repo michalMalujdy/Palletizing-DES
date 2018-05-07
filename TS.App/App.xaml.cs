ce1ed383a053c59f384b2934dbf3e37d96d24386
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-05-07 18:22:30 +0200

    Automapper initialization

--- TS.App/App.xaml.cs
+++ TS.App/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using TS.Infrastructure.AppStart;
 
 namespace TS.App
 {
@@ -30,6 +31,8 @@ namespace TS.App
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+
+            AutomapperConfig.Initialize();
         }
 
         /// <summary>
