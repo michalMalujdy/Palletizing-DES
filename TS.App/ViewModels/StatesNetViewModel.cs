9f7cd6fa663b3456a228662ca218774bf081d519
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-05-07 19:47:57 +0200

    Binding states text boxes and font sizes

--- TS.App/ViewModels/StatesNetViewModel.cs
+++ TS.App/ViewModels/StatesNetViewModel.cs
@@ -1,9 +1,20 @@
-﻿using TS.Infrastructure.Services;
+﻿using TS.App.ViewModels.Common;
+using TS.Infrastructure.Services;
 
 namespace TS.App.ViewModels
 {
-    public class StatesNetViewModel
+    public class StatesNetViewModel : BaseViewModel
     {
+        public string GpText => _statesNetService.StatesNet.CurrentState.Gp;
+        public string Sl1Text => _statesNetService.StatesNet.CurrentState.Sl1;
+        public string Sl2Text => _statesNetService.StatesNet.CurrentState.Sl2;
+        public string UpText => _statesNetService.StatesNet.CurrentState.Up;
+        public string SvText => _statesNetService.StatesNet.CurrentState.Sv;
+
+        public int LargeFontSize => 55;
+        public int MediumFontSize => 40;
+
+
         private readonly StatesNetService _statesNetService;
 
         public StatesNetViewModel(StatesNetService statesNetService)
