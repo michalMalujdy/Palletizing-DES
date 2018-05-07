3919b1af035dc4fe4ce340232daade3a7b545f74
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-05-07 19:47:36 +0200

    Add baseViewModel

---
+++ TS.App/ViewModels/Common/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using TS.App.Tools;
+
+namespace TS.App.ViewModels.Common
+{
+    public class BaseViewModel : ViewModelBase
+    {
+        public override async void RaisePropertyChanged(string propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                await UiSynchronizer.Run(() =>
+                    base.RaisePropertyChanged(propertyName));
+            }
+        }
+    }
+}
