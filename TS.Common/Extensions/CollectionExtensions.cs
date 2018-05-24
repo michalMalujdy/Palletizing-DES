f7a9067296e52975cc15e99bd27317384b45f86b
Michał Malujdy <michal.c.malujdy@student.put.poznan.pl>
2018-05-24 18:04:04 +0200

    CollectionExtensions

---
+++ TS.Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TS.Common.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this ICollection<T> collection) where T : class
+        {
+            return collection == null || !collection.Any();
+        }
+    }
+}
